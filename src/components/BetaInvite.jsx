9b21047cd5629900109dea5af751b21d5da8197e
ZiadRefaey <99500161+ZiadRefaey@users.noreply.github.com>
2023-10-21 15:34:37 +0300

    BetaInvite

---
+++ src/components/BetaInvite.jsx
@@ -0,0 +1,9 @@
+import ColoredBar from "./ColoredBar";
+export default function BetaInvite() {
+  return (
+    <div className="relative flex items-center justify-between">
+      <ColoredBar className={"md:top-0"} />
+      <h4 className="text-custom-48 text-white uppercase leading-40 tracking-widest"></h4>
+    </div>
+  );
+}
--- src/components/ColoredBar.jsx
+++ src/components/ColoredBar.jsx
@@ -1,3 +1,7 @@
 export default function ColoredBar({ hidden, className }) {
-  return <div className={`gradient absolute ${className} ${hidden}`}></div>;
+  return (
+    <div
+      className={`gradient absolute top-0 left-[33px] h-[6px] w-[128px] md:h-[304px] md:w-[6px] md:top-[173px] md:left-0 ${className} ${hidden}`}
+    ></div>
+  );
 }
